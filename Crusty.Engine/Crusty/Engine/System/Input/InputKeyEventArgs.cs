4ae53ff939ca7c4cb9e27b5d8f39c78ef5b97f1a
LipkeGu <contact-bln@fblipke.de>
2023-04-04 01:47:48 +0200

    Generalize: KeyInputEventArgs

---
+++ Crusty.Engine/Crusty/Engine/System/Input/InputKeyEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Input;
+
+namespace Crusty.Engine
+{
+	public class InputKeyEventArgs
+	{
+		public Key Key { get; private set; }
+		public bool Pressed { get; private set; }
+		public bool ShiftPressed { get; private set; }
+		public bool AltPressed { get; private set; }
+
+		public float DeltaTIme { get; private set; }
+
+		public InputKeyEventArgs(Key key, bool isPressed, double deltatime, bool alt, bool shift)
+		{
+			ShiftPressed = shift;
+			AltPressed = alt;
+
+			DeltaTIme = (float)deltatime;
+			Key = key;
+			Pressed = isPressed;
+		}
+	}
+}
