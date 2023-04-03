b956a0dd4ea0147eb3e400e01dcb4f8918297f2c
LipkeGu <contact-bln@fblipke.de>
2023-04-04 01:50:57 +0200

    Add Mouse Input EventHandler

---
+++ Crusty.Engine/Crusty/Engine/System/Input/InputMButtonEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using OpenTK.Input;
+
+namespace Crusty.Engine
+{
+	public class InputButtonEventArgs
+	{
+		public MouseButton Button { get; private set; }
+		public bool Pressed { get; private set; }
+
+		public CursorPosition Position { get; private set; }
+		public Vector3 RayPosition { get; private set; }
+
+		public float DeltaTIme { get; private set; }
+
+		public InputButtonEventArgs(MouseButton button, CursorPosition position, Vector3 CursorRay, bool isPressed, double deltatime)
+		{
+			DeltaTIme = (float)deltatime;
+			Pressed = isPressed;
+			RayPosition = CursorRay;
+			Position = position;
+			Button = button;
+		}
+	}
+}
